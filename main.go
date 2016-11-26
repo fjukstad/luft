999666c25c3ca15a6d9e1f1ea773a35071606545
Bjørn Fjukstad <bjorn@cs.uit.no>
2016-11-26 15:23:31 -0500

    add test log data

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 	"text/template"
 
 	"github.com/fjukstad/luftkvalitet"
@@ -12,13 +16,15 @@ import (
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", IndexHandler)
-	r.HandleFunc("/aqis/{id}", AquisGeoJSON)
-	r.HandleFunc("/public/{folder}/{file}", PublicHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", IndexHandler)
+	mux.HandleFunc("/aqis/", AquisGeoJSON)
+	mux.HandleFunc("/logs/", LogHandler)
+
+	http.Handle("/public", http.FileServer(http.Dir("public")))
 
 	fmt.Println("Server started on localhost:8000")
-	err := http.ListenAndServe(":8000", r)
+	err := http.ListenAndServe(":8000", mux)
 
 	if err != nil {
 		fmt.Println(err)
@@ -27,9 +33,65 @@ func main() {
 
 }
 
+func LogHandler(w http.ResponseWriter, r *http.Request) {
+	filename := "data/test-data.csv"
+	f, err := os.Open(filename)
+	if err != nil {
+		w.Write([]byte("Could not open data file " + err.Error()))
+		return
+	}
+
+	reader := csv.NewReader(f)
+	records, err := reader.ReadAll()
+	if err != nil {
+		w.Write([]byte("Could not parse csv file " + err.Error()))
+		return
+	}
+
+	fc := geojson.NewFeatureCollection()
+	for _, record := range records {
+		if len(record) < 6 {
+			w.Write([]byte("error parsing csv"))
+			return
+		}
+
+		date := record[0]
+		lat, err := strconv.ParseFloat(record[1], 64)
+		if err != nil {
+			w.Write([]byte("error parsing float " + err.Error()))
+			return
+		}
+		long, err := strconv.ParseFloat(record[2], 64)
+		if err != nil {
+			w.Write([]byte("error parsing float " + err.Error()))
+			return
+		}
+		ppm := record[3]
+		humid := record[4]
+		temp := record[5]
+
+		geom := geojson.NewPointGeometry([]float64{long, lat})
+		f := geojson.NewFeature(geom)
+		f.SetProperty("date", date)
+		f.SetProperty("ppm", ppm)
+		f.SetProperty("humid", humid)
+		f.SetProperty("temp", temp)
+		fc = fc.AddFeature(f)
+	}
+	b, err := fc.MarshalJSON()
+
+	if err != nil {
+		fmt.Println(err)
+		w.Write([]byte("Could not marshal geojson"))
+		return
+	}
+
+	w.Write(b)
+}
+
 func AquisGeoJSON(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
+	p := r.URL.Path
+	id := strings.Trim(p, "/aqis/")
 	id = "{" + id + "}"
 
 	aqis, err := luftkvalitet.GetAqis(id)
