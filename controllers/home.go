1abbd737ee8ef780219f4fdae62a3093838f8419
Nina Angelvik <nina.angelvik@gmail.com>
2017-11-13 12:04:44 +0100

    Upload page completed

--- controllers/home.go
+++ controllers/home.go
@@ -6,6 +6,9 @@ import (
 	"text/template"
 	"encoding/json"
 	"bytes"
+	"mime/multipart"
+	"sync"
+	// "strings"
 )
 
 
@@ -60,16 +63,21 @@ type FileInfo struct {
 	Contents 		string
 }
 
+type PostResponse struct {
+	StatusCode 	int
+	Filename 	 	string
+}
 
-func PostFileHandler(w http.ResponseWriter, r *http.Request) {
-	// _, header, err := r.FormFile("uploadFile")
-	file, header, err := r.FormFile("uploadFile")
-	if err != nil {
+func sendFile(fh *multipart.FileHeader, c chan PostResponse, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+	f, err := fh.Open()
+  if err != nil {
 		fmt.Println(err)
 	}
-
+  
 	var buf bytes.Buffer
-	fileSize, err := buf.ReadFrom(file)
+	fileSize, err := buf.ReadFrom(f)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -77,19 +85,18 @@ func PostFileHandler(w http.ResponseWriter, r *http.Request) {
 	url := "http://localhost:8080/api/upload"
 	
 	fi := FileInfo {
-		Filename: header.Filename,
+		Filename: fh.Filename,
 		Size: fileSize,
-		ContentType: header.Header["Content-Type"][0], 
+		ContentType: fh.Header["Content-Type"][0], 
 		Contents: buf.String(),
 	}
 
-	f, err := json.Marshal(fi)
+	file, err := json.Marshal(fi)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(f))
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(file))
   req.Header.Set("X-Custom-Header", "myvalue")
   req.Header.Set("Content-Type", "application/json")
 
@@ -98,15 +105,58 @@ func PostFileHandler(w http.ResponseWriter, r *http.Request) {
   if err != nil {
     panic(err)
   }
+	
+	response := PostResponse{
+		StatusCode: resp.StatusCode,
+		Filename: fh.Filename,
+	}
 
-  defer resp.Body.Close()
+	c <- response
+}
 
-	http.Redirect(w,r, "/alert", http.StatusFound)
+
+
+func PostFileHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+		c := make(chan PostResponse, 10)
+		var wg sync.WaitGroup
+		var succResponses []string
+		var failResponses []string
+
+		r.ParseMultipartForm(32 << 20) // 32MB is the default used by FormFile
+		fhs := r.MultipartForm.File["uploadFile"]
+
+		for _, fh := range fhs {
+			wg.Add(1)
+			go sendFile(fh, c, &wg)
+		}
+
+		wg.Wait()
+		for i := 0; i < len(fhs); i++ {
+			response := <- c
+			if (response.StatusCode == 200) {
+				succResponses = append(succResponses, response.Filename)
+			} else {
+				failResponses = append(failResponses, response.Filename)
+			}
+		}
+		close(c)
+		
+		if (len(failResponses) == 0) {
+			fmt.Fprintf(w, "Opplasting vellykket!")
+			return
+		}	else {
+			fmt.Fprintf(w, "Følgene filer ble ikke lastet opp: ")
+			return
+		}
+ 	}
 }
 
 var alertTemplate = template.Must(template.ParseFiles("views/base.html",
 	"views/header.html", "views/navbar.html",
 	"views/alert.html", "views/footer.html"))
+
 
 func AlertHandler(w http.ResponseWriter, r *http.Request) {
 	err := alertTemplate.Execute(w, nil)
